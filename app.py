042b900759fe6f3b871f2706d68e43f5b30dcd92
totytinasia-commits <totytinasia@gmail.com>
2026-09-09 15:17:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -607,9 +607,9 @@ with center_col:
         config_cert_cols = {}
         for i, col_name in enumerate(df_certificazioni.columns):
             if i == 0:
-                config_cert_cols[col_name] = st.column_config.TextColumn(col_name, width="small", pinned=True)
+                config_cert_cols[col_name] = st.column_config.TextColumn(col_name, width="medium", pinned=True)
             else:
-                config_cert_cols[col_name] = st.column_config.TextColumn(col_name, width="medium")
+                config_cert_cols[col_name] = st.column_config.TextColumn(col_name, width="small")
 
         with st.container():
             st.dataframe(
