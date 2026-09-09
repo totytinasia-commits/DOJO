236b26ad87f22ed024dad84f35f16a41f5901fd5
totytinasia-commits <totytinasia@gmail.com>
2026-09-09 17:43:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -869,7 +869,7 @@ with center_col:
 
         st.markdown("---")
 
-        elif st.session_state.stat_tab == "⚙️ SETTINGS":
+        if st.session_state.stat_tab == "⚙️ SETTINGS":
             st.markdown("<div style='background-color: #0e1117; border: 2px solid #262730; border-radius: 12px; padding: 15px;'>", unsafe_allow_html=True)
             st.markdown("### ⚙️ Settings")
 
