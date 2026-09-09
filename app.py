8d35903a8e706b1208935f3bba5e4716aa200e7e
totytinasia-commits <totytinasia@gmail.com>
2026-09-09 15:09:24 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -529,7 +529,7 @@ with center_col:
                 config_cols[col_name] = st.column_config.ProgressColumn(
                     col_name,
                     min_value=0,
-                    max_value=100,
+                    max_value=80,
                     format="%s",
                     width="small"
                 )
